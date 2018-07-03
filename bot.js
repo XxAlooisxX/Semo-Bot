4a2a6c80138291c4518f5914888690274f52cff1
XxAlooisxX <40740670+XxAlooisxX@users.noreply.github.com>
2018-07-03 14:31:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -323,6 +323,24 @@ client.on('message', message => {
     }
 });
 
+if (message.content === '.Rules') {
+         let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)    
+      .addField("**:globe_with_meridians: القوانين**","** **")
+      .addField("***ارسل الرسائل في الرومات المخصصة**")
+      .addField("***id :chart_with_downwards_trend:**","**عرض سرعه اتصال البوت**")
+      .addField("***avatar :camping:**","**لـ صور الشخص المختار**")
+      .addField("***roll :1234:**","**لـ القرعه من 1 - 100**")
+      .addField("***server :recycle:**","**لـ معلومات السيرفر**")
+      .addField("***roles :medal: **","**مسح محادثات الشات**")
+      .addField("***say :arrows_counterclockwise:**","**لـ يكرر الكلام اللى تقوله**")
+      .addField("***time :alarm_clock:**","**لـ معرفة الساعة**")
+      .addField("***date **","**لـ معرفة التاريخ**")
+.setColor('RANDOM')
+  message.author.sendEmbed(embed);
+    }
+});
+
 
 
 
