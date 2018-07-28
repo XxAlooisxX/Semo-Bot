106d33e637ebce38ff31492830ffe8a7558dbf6f
XxAlooisxX <40740670+XxAlooisxX@users.noreply.github.com>
2018-07-28 21:36:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '*';
-const discord_token = "NDcyODMxNDQ4NTcxOTY5NTM3.Dj5Gbg.KLzsTExpsWJOiZTJP-xvqPWDV-g";
+const discord_token = "process.env.BOT_TOKEN";
 client.login(discord_token);
 client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
@@ -235,4 +235,4 @@ client.on('message', function(message) {
 
 
 
-client.login(NDcyODMxNDQ4NTcxOTY5NTM3.Dj5Gbg.KLzsTExpsWJOiZTJP-xvqPWDV-g);
+client.login(process.env.BOT_TOKEN);
